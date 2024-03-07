dc17026f51e5e2b63a099fa2f7bdfbaf9cf2a929
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2024-03-06 18:15:13 -0600

    Tests

--- api/location/tests.py
+++ api/location/tests.py
@@ -1,36 +1,66 @@
-from django.test import TestCase
-from api import models 
+from rest_framework.test import APITestCase
+from rest_framework import status
+from location.models import Location
+from trip.models import Trip
+from user.models import Client
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase, force_authenticate
 
-class LocationTests(TestCase):
+class LocationTests(APITestCase):
 
-    def test_tag_model_str(self)
-        task = models.Task.objects.create
-        (
-            addr_address = "https://api.chases-server.com/api/v1/users",
-            business_status = "test",
-            formatted_address = "123 Anywhere USA",
-            formatted_phone_number = "123-456-7890",
-            icon = "https://api.chases-server.com/api/v1/users",
-            icon_background_color = "BLUE"
-            icon_mask_base_uri = "https://api.chases-server.com/api/v1/users",
-            international_phone_number = "020-1234-1234",
-            name = "Steve",
-            place_id = "placeID",
-            rating = "5",
-            reference = "test",
-            url = "https://api.chases-server.com/api/v1/users",
-            user_ratings_total = "13",
-            vicinity = "test",
-            website = "https://api.chases-server.com/api/v1/users",
-            geometry_location_lat = "100.5",
-            geometry_location_lng = "73.4",
-            geometry_viewport_northeast_lat = "21.6",
-            geometry_viewport_northeast_lng = "158.9",
-            geometry_viewport_northeast_lng = "210.4",
-            geometry_viewport_northeast_lng = "11.9",
-            plus_code_compound_code = "test code",
-            plus_code_global_code = "test global",
-        )
+    def setUp(self):
+        self.user = Client.objects.create_user(email='test@example.com', password='testpassword', username='testuser')
+        self.trip_instance =  Trip.objects.create(name="Test Trip", client_id=1)
+        
+        self.location_data = {
+            'trip': self.trip_instance,
+            'addr_address': "https://api.chases-server.com/api/v1/users",
+            'business_status': "test",
+            'formatted_address': "123 Anywhere USA",
+            'formatted_phone_number': "123-456-7890",
+            'icon': "https://api.chases-server.com/api/v1/users",
+            'icon_background_color': "BLUE",
+            'icon_mask_base_uri': "https://api.chases-server.com/api/v1/users",
+            'international_phone_number': "020-1234-1234",
+            'name': "Steve",
+            'place_id': "placeID",
+            'rating': 5,
+            'reference': "test",
+            'url': "https://api.chases-server.com/api/v1/users",
+            'user_ratings_total': 13,
+            'vicinity': "test",
+            'website': "https://api.chases-server.com/api/v1/users",
+            'latitude': 100.5,
+            'longitude': 73.4,
+            'compound_code': "test code",
+            'global_code': "test global",
+        }
 
-        self.assertEqual(unicode(task), task.None)
-# Create your tests here.
+    def test_create_location(self):
+        location = Location.objects.create(**self.location_data)
+        created_location = Location.objects.get(pk=location.id)
+
+        self.assertEqual(created_location.addr_address, self.location_data['addr_address'])
+        self.assertEqual(created_location.business_status, self.location_data['business_status'])
+        self.assertEqual(created_location.trip, self.trip_instance)
+
+    def test_read_location(self):
+        location = Location.objects.create(**self.location_data)
+        url = reverse('locations-detail', kwargs={'trip_pk': self.trip_instance.id, 'pk': location.id})
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['addr_address'], self.location_data['addr_address'])
+        
+    def test_delete_location(self):
+        location = Location.objects.create(**self.location_data)
+        url = reverse('locations-detail', kwargs={'trip_pk': location.trip.id, 'pk': location.id})
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.assertRaises(Location.DoesNotExist):
+            Location.objects.get(pk=location.id)
+
+
+
--- api/trip/tests.py
+++ api/trip/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from trip.models import Trip
+from user.models import Client
 
-# Create your tests here.
+class TripTests(APITestCase):
+
+    def setUp(self):
+        self.user = Client.objects.create_user(email='test@example.com', password='testpassword', username='testuser')
+        
+        self.trip_instance = Trip.objects.create(client=self.user, name='Test Trip', description='Test description', favorite=False)
+
+        self.trip_data = {
+            'client': self.user.id,
+            'name': 'Test Trip',
+            'description': 'This is a test trip.',
+            'favorite': True,
+        }
+
+    def test_create_trip(self):
+        url = reverse('trips-list')  
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, self.trip_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_read_trip(self):
+        url = reverse('trips-detail', kwargs={'pk': self.trip_instance.id})
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Test Trip')
+        self.assertEqual(response.data['description'], 'Test description')
+
+    def test_update_trip(self):
+        url = reverse('trips-detail', kwargs={'pk': self.trip_instance.id})
+        self.client.force_authenticate(user=self.user)
+
+        updated_data = {
+            'name': 'Updated Trip',
+            'description': 'Updated description',
+            'favorite': True,
+        }
+
+        response = self.client.put(url, updated_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_trip = Trip.objects.get(pk=self.trip_instance.id)
+        self.assertEqual(updated_trip.name, 'Updated Trip')
+        self.assertEqual(updated_trip.description, 'Updated description')
+        self.assertEqual(updated_trip.favorite, True)
+
+    def test_delete_trip(self):
+        url = reverse('trips-detail', kwargs={'pk': self.trip_instance.id})
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        with self.assertRaises(Trip.DoesNotExist):
+            Trip.objects.get(pk=self.trip_instance.id)
