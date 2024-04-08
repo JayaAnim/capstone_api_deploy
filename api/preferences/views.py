cdc4a6a514d053d6c66ace4d76e44f69ff68fc94
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2024-04-07 20:28:12 -0500

    Update views.py

--- api/preferences/views.py
+++ api/preferences/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import json
+
+from rest_framework import permissions, viewsets
+from rest_framework import permissions, status, viewsets, views
+from rest_framework.response import Response
+
+from preferences import models
+
+class PreferenceViewSet(viewsets.ModelViewSet):
+    # Must be authenticated to create, update, or read preferences
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = models.Preference.objects.all()
+    serializer_class = serializers.PreferenceSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(trip=self.kwargs['trip_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(trip=self.kwargs['trip_pk'])
 
 # Create your views here.
